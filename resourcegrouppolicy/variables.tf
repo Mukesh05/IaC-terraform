6e8d950608bf1dbea391400835dc63c9cff5d82a
Mukesh Kumar <mysticmukesh@gmail.com>
2024-11-17 11:24:18 +0530

    Update variables.tf

---
+++ resourcegrouppolicy/variables.tf
@@ -0,0 +1,12 @@
+variable "policyname" {
+    type = list(string)
+}
+variable "rgname" {
+    type = list(string)
+}
+variable "subscription_id" {
+    type = string
+}
+variable "parameters" {
+    type = any
+}
