9e783a1e2d2f87b1be29103fc4ef689af16dad8a
Mukesh Kumar <mysticmukesh@gmail.com>
2024-11-17 11:25:18 +0530

    Update variables.tf

---
+++ subscriptionpolicy/variables.tf
@@ -0,0 +1,12 @@
+variable "policyname" {
+    type = list(string)
+}
+variable "subsid" {
+    type = list(string)
+}
+variable "parameters" {
+  /*type = list(any)
+  (map(object({
+    value = any
+  })))*/
+}
